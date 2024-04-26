e95022aff18178f2301207f92ff11c1f7179b153
Jeff Wang <jeffwang114@gmail.com>
2024-04-26 13:52:57 -0500

    updated emergency stop

--- EmergencyStop.py
+++ EmergencyStop.py
@@ -1,8 +1,10 @@
-from Gantry import Gantry
+from Gantry3 import Gantry3
 from HR8825zero import HR8825zero
+from Coordinate import Coordinate
 
 Motor1 = HR8825zero(dir_pin=13, step_pin=19, enable_pin=12)
 Motor2 = HR8825zero(dir_pin=24, step_pin=18, enable_pin=4)
-Gantry = Gantry(Motor1, Motor2)
+coord = Coordinate(200)
+Gantry = Gantry3(Motor1, Motor2, coord)
 
 Gantry.stop()
