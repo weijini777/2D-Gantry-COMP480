eb9b5a114c7857363c7b1ecacdd955b28675495e
Jeff Wang <jeffwang114@gmail.com>
2024-04-26 16:24:10 -0500

    test the diagonals

--- test.py
+++ test.py
@@ -14,43 +14,43 @@ forward = [100,299]
 backward = [100,1]
 left = [1, 100]
 right = [200, 99]
-diag = [297, 298]
-diag1 = [200, 198]
-diag2 = [275, 225]
-diag3 = [225, 276]
+lu = [50, 150]
+ru = [149, 150]
+ld = [49, 51]
+rd = [150, 50]
 
-# Gantry.travel(center, diag)
-# Gantry.stop()
-# time.sleep(0.5)
-
-# Gantry.travel(center, diag1)
-# Gantry.stop()
-# time.sleep(0.5)
-
-# Gantry.travel(center, diag2)
-# Gantry.stop()
-# time.sleep(0.5)
-
-# Gantry.travel(center, diag3)
-# Gantry.stop()
-# time.sleep(0.5)
-
-Gantry.travel(center, forward)
+Gantry.travel(center, lu)
 Gantry.stop()
-time.sleep(1)
+time.sleep(0.5)
 
-print("Does it make it past the first chain of tests")
-
-Gantry.travel(center, backward)
+Gantry.travel(center, rd)
 Gantry.stop()
-time.sleep(1)
+time.sleep(0.5)
 
-Gantry.travel(center, left)
+Gantry.travel(center, ru)
 Gantry.stop()
-time.sleep(1)
+time.sleep(0.5)
 
-Gantry.travel(center, right)
+Gantry.travel(center, ld)
 Gantry.stop()
-time.sleep(1)
+time.sleep(0.5)
+
+# Gantry.travel(center, forward)
+# Gantry.stop()
+# time.sleep(1)
+
+# print("Does it make it past the first chain of tests")
+
+# Gantry.travel(center, backward)
+# Gantry.stop()
+# time.sleep(1)
+
+# Gantry.travel(center, left)
+# Gantry.stop()
+# time.sleep(1)
+
+# Gantry.travel(center, right)
+# Gantry.stop()
+# time.sleep(1)
 
 exit()
