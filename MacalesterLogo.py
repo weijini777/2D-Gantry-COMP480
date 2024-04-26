1c6878dac3f8e6f7bb9ccd7aa7a22443fcd5a519
Jeff Wang <jeffwang114@gmail.com>
2024-04-26 16:49:45 -0500

    try again

--- MacalesterLogo.py
+++ MacalesterLogo.py
@@ -31,7 +31,8 @@ sequence = [[a,e1], [e1,h4], [h4,h3], [h3,f1], [f1,b2], [d,h1], [h1,h2], [h2,e2]
 
 for i in sequence:
     Gantry.travel(i[0],i[1])
-    time.sleep(0.7)
     Gantry.stop()
+    time.sleep(2)
+    
     
 exit()
