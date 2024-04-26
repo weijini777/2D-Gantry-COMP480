f35f87f1223dafb8845c7e81187a5cf8cc584147
Jeff Wang <jeffwang114@gmail.com>
2024-04-26 16:19:56 -0500

    figure out the directions

--- test.py
+++ test.py
@@ -9,31 +9,31 @@ Motor2 = HR8825zero(dir_pin=24, step_pin=18, enable_pin=4)
 coord = Coordinate(300)
 Gantry = Gantry3(Motor1, Motor2, coord)
 # test out the gantry class
-center = [250, 250]
-forward = [249,300]
-backward = [254,200]
-left = [180, 250]
-right = [320, 253]
+center = [100, 100]
+forward = [100,299]
+backward = [100,1]
+left = [1, 100]
+right = [200, 99]
 diag = [297, 298]
 diag1 = [200, 198]
 diag2 = [275, 225]
 diag3 = [225, 276]
 
-Gantry.travel(center, diag)
-Gantry.stop()
-time.sleep(0.5)
+# Gantry.travel(center, diag)
+# Gantry.stop()
+# time.sleep(0.5)
 
-Gantry.travel(center, diag1)
-Gantry.stop()
-time.sleep(0.5)
+# Gantry.travel(center, diag1)
+# Gantry.stop()
+# time.sleep(0.5)
 
-Gantry.travel(center, diag2)
-Gantry.stop()
-time.sleep(0.5)
+# Gantry.travel(center, diag2)
+# Gantry.stop()
+# time.sleep(0.5)
 
-Gantry.travel(center, diag3)
-Gantry.stop()
-time.sleep(0.5)
+# Gantry.travel(center, diag3)
+# Gantry.stop()
+# time.sleep(0.5)
 
 Gantry.travel(center, forward)
 Gantry.stop()
