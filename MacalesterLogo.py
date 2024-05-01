dedca1cf3d7bb381cb24c85771f62040ce6e7240
Jeff Wang <jeffwang114@gmail.com>
2024-04-30 22:06:54 -0500

    lets test out the macalester logo!

--- MacalesterLogo.py
+++ MacalesterLogo.py
@@ -4,8 +4,8 @@ from HR8825zero import HR8825zero
 from Gantry3 import Gantry3
 from Coordinate import Coordinate
 
-Motor1 = HR8825zero(dir_pin=13, step_pin=19, enable_pin=12)
-Motor2 = HR8825zero(dir_pin=24, step_pin=18, enable_pin=4)
+Motor2 = HR8825zero(dir_pin=13, step_pin=19, enable_pin=12)
+Motor1 = HR8825zero(dir_pin=24, step_pin=18, enable_pin=4)
 coordinate = Coordinate(200)
 Gantry = Gantry3(Motor1, Motor2, coordinate)
 
